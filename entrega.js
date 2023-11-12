bae56d64aa360742b9fd9c934ecf11c4f5c89d3f
Kevin Caputo <kev.caputo63@gmail.com>
2023-11-12 17:14:35 -0300

    lajuanita2

--- entrega.js
+++ entrega.js
@@ -1,6 +1,7 @@
 const SI = "si";
 const NO = "no";
 
+
 let fruta;
 let precio;
 let kilogramos;
@@ -11,12 +12,27 @@ let continuar;
 let ticket = "Nombre     Precio      Cantidad     Subtotal      \n";
 
 nombre = prompt(" Bienvenido a Fruteria La Juanita!\n Por favor, Ingrese su nombre: ");
+while (!/^[a-zA-Z]+$/.test(nombre)) {
+    alert("Por favor, ingrese un nombre válido (solo letras).");
+    nombre = prompt("Por favor, ingrese su nombre: ");
+}
 
 do {
     fruta = prompt(`${nombre}, Ingresa el nombre de la fruta: `);
+    while (!/^[a-zA-Z]+$/.test(fruta)) {
+        alert("Por favor, ingrese un nombre de fruta válido (solo letras).");
+        fruta = prompt(`${nombre}, ingrese el nombre de la fruta: `);
+    }
     precio = +prompt(" Ingresa el precio: ");
+    while (isNaN(precio) || precio <= 0) {
+        alert("Por favor, ingrese un valor numérico mayor a 0 para el precio.");
+        precio = parseFloat(prompt("Ingrese el precio: "));
+    }
     kilogramos = +prompt("Ingresa la cantidad de kg: ");
-
+    while (isNaN(kilogramos) || kilogramos <= 0) {
+        alert("Por favor, ingrese un valor numérico mayor a 0 para la cantidad de kg.");
+        kilogramos = parseFloat(prompt("Ingrese la cantidad de kg: "));
+    }
     fila = `${fruta}            $${precio}             ${kilogramos}Kg             $${kilogramos*precio} \n`;
     ticket=  ticket + fila 
     total = total + precio * kilogramos - descuento
